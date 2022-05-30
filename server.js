6e3b895f1bc38b21ae141dd111fdccf1b31e7ed9
Icepick <remi.jara@laposte.net>
2022-05-30 20:33:21 +0200

    both methods in same branch

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const EXPRESS = require('express');
+const EXPRESS = require("express");
 const app = EXPRESS();
-app.use('/', EXPRESS.static(__dirname + "/public"));
+app.use("/", EXPRESS.static(__dirname + "/drag-and-drop/public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/drag-and-drop/public/index.html");
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000');
+    console.log("Server is running on port http://localhost:3000");
 });
