ad8b4074a8ffb5efa1cbd022e2f0e19ed96de5a1
icepick4 <remi.jara@laposte.net>
2022-05-30 11:30:20 +0200

    removing useless code + display moves

--- public/js/main.js
+++ public/js/main.js
@@ -96,6 +96,8 @@ function unselectAll() {
 function play() {
     //clear div with id "disks"
     CONTAINER_DISKS.innerHTML = "";
+    unselectAll();
+    won.innerHTML = "Moves : 0";
     if (input != null) {
         var n = Number(input.value);
         if (n > 0 && n <= 7) {
@@ -103,14 +105,6 @@ function play() {
             hanoi.draw();
         }
     }
-    won.style.display = "none";
-}
-function getMousePos(canvas, e) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-    };
 }
 class Hanoi {
     constructor(n) {
@@ -121,16 +115,16 @@ class Hanoi {
             this.towers.push(disk_array);
         }
         for (var i = 0; i < this.n; i++) {
-            //random color for fillStyle
-            var r = Math.floor(Math.random() * 255);
-            var g = Math.floor(Math.random() * 255);
-            var b = Math.floor(Math.random() * 255);
-            this.towers[0].push(new Disk(this.n - i, "rgb(" + r + ", " + g + ", " + b + ")"));
+            this.towers[0].push(this.n - i);
             //i into string
             var str = (this.n - i).toString();
             //create a new div with id disk_i
             var div = document.createElement("div");
             div.id = "disk_" + str;
+            //random color for fillStyle
+            var r = Math.floor(Math.random() * 255);
+            var g = Math.floor(Math.random() * 255);
+            var b = Math.floor(Math.random() * 255);
             div.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
             div.style.width = (this.n - i) * 60 + "px";
             div.style.height = "45px";
@@ -143,10 +137,6 @@ class Hanoi {
         this.moves = 0;
         this.solved = false;
         this.clicked1 = null;
-        this.pics = [];
-        // this.pics.push(new Pic(this.canvas.width / 2 - 450));
-        // this.pics.push(new Pic(this.canvas.width / 2));
-        // this.pics.push(new Pic(this.canvas.width / 2 + 450));
     }
     setClicked(i) {
         this.clicked1 = i;
@@ -159,66 +149,40 @@ class Hanoi {
             return true;
         }
         else {
-            return this.towers[from][this.towers[from].length - 1].width < this.towers[to][this.towers[to].length - 1].width;
+            return this.towers[from][this.towers[from].length - 1] < this.towers[to][this.towers[to].length - 1];
         }
     }
     move(from, to) {
         console.log("j'ai boug");
         this.towers[to].push(this.towers[from].pop());
         this.moves++;
+        won.innerHTML = "Moves : " + this.moves.toString();
         if (this.towers[2].length == this.n) {
             this.solved = true;
             //display #won 
-            won.style.display = "block";
+            won.innerHTML = "You won in " + this.moves.toString() + " moves!";
         }
     }
     draw() {
-        console.log("je dessin le premier pic");
         //draw the disks on the lines
         for (var i = 0; i < this.towers[0].length; i++) {
             console.log(this.towers[0].length - i);
-            var div = document.getElementById("disk_" + (this.towers[0][i].width).toString());
+            var div = document.getElementById("disk_" + (this.towers[0][i]).toString());
             div.style.bottom = (i) * 45 + 30 - 10 + "px";
             div.style.left = screen.width / 6 - div.offsetWidth / 2 + 5 + "px";
         }
         for (var i = 0; i < this.towers[1].length; i++) {
-            var div = document.getElementById("disk_" + (this.towers[1][i].width).toString());
+            var div = document.getElementById("disk_" + (this.towers[1][i]).toString());
             div.style.bottom = (i) * 45 + 30 - 10 + "px";
             div.style.left = screen.width / 2 - div.offsetWidth / 2 - 5 + "px";
         }
         for (var i = 0; i < this.towers[2].length; i++) {
-            var div = document.getElementById("disk_" + (this.towers[2][i].width).toString());
+            var div = document.getElementById("disk_" + (this.towers[2][i]).toString());
             div.style.bottom = (i) * 45 + 30 - 10 + "px";
             div.style.left = screen.width / 6 * 5 - div.offsetWidth / 2 - 7.5 + "px";
         }
     }
     isSolved() {
         return this.solved;
-    }
-}
-class Disk {
-    constructor(width, color) {
-        this.width = width;
-        this.color = color;
-        this.x = null;
-        this.y = null;
-    }
-    setX(x) {
-        this.x = x;
-    }
-    setY(y) {
-        this.y = y;
-    }
-}
-class Pic {
-    constructor(x) {
-        this.x = x;
-        this.y = 250;
-        this.width = 10;
-        this.height = 600;
-        this.color = "black";
-    }
-    setColor(color) {
-        this.color = color;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -102,7 +102,8 @@ function unselectAll(){
 function play(){
     //clear div with id "disks"
     CONTAINER_DISKS.innerHTML = "";
-
+    unselectAll();
+    won.innerHTML = "Moves : 0";
     if(input != null){
         var n : number = Number(input.value); 
         if(n > 0 && n <= 7){
@@ -110,58 +111,49 @@ function play(){
             hanoi.draw();
         } 
     }
-    won.style.display = "none";
-}
-
-function getMousePos(canvas: HTMLCanvasElement, e: MouseEvent) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top
-    };
 }
 
 class Hanoi{
     n: number;
     clicked1 : number | null ;
-    towers : Array<Array<Disk>>;
+    towers : Array<Array<number>>;
     moves : number;
     solved : boolean;
-    pics : Array<Pic>;
     constructor(n : number){
         this.n = n;
         this.towers = [];
         for(var i = 0; i < 3; i++){
-            let disk_array : Array<Disk> = [];
+            let disk_array : Array<number> = [];
             this.towers.push(disk_array);
         }
         for(var i = 0; i < this.n; i++){
-            //random color for fillStyle
-            var r = Math.floor(Math.random() * 255);
-            var g = Math.floor(Math.random() * 255);
-            var b = Math.floor(Math.random() * 255);
-            this.towers[0].push(new Disk(this.n - i, "rgb(" + r + ", " + g + ", " + b + ")"));
+            this.towers[0].push(this.n - i);
+
             //i into string
             var str = (this.n-i).toString();
             //create a new div with id disk_i
             var div = document.createElement("div");
             div.id = "disk_" + str;
+
+            //random color for fillStyle
+            var r = Math.floor(Math.random() * 255);
+            var g = Math.floor(Math.random() * 255);
+            var b = Math.floor(Math.random() * 255);
             div.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+
             div.style.width = (this.n - i) * 60 + "px";
             div.style.height = "45px";
             div.style.position = "absolute";
+
             div.style.bottom = (i) * 45 + 30 - 10 + "px";
             div.style.left = screen.width / 6 - (this.n - i) * 30 + 2.5 + "px";
             div.style.zIndex = "1";
+
             CONTAINER_DISKS.appendChild(div);
         }
         this.moves = 0;
         this.solved = false;
         this.clicked1 = null;
-        this.pics = [];
-        // this.pics.push(new Pic(this.canvas.width / 2 - 450));
-        // this.pics.push(new Pic(this.canvas.width / 2));
-        // this.pics.push(new Pic(this.canvas.width / 2 + 450));
     }
 
     setClicked(i : number){
@@ -176,38 +168,37 @@ class Hanoi{
             return true;
         }
         else{
-            return this.towers[from][this.towers[from].length - 1].width < this.towers[to][this.towers[to].length - 1].width;
+            return this.towers[from][this.towers[from].length - 1] < this.towers[to][this.towers[to].length - 1];
         }
     }
 
     move(from : number, to : number){
         console.log("j'ai boug");
-        this.towers[to].push(this.towers[from].pop() as Disk);
+        this.towers[to].push(this.towers[from].pop() as number);
         this.moves++;
+        won.innerHTML = "Moves : " + this.moves.toString();
         if(this.towers[2].length == this.n){
             this.solved = true;
             //display #won 
-            won.style.display = "block";
+            won.innerHTML = "You won in " + this.moves.toString() + " moves!";
         }
     }
 
     draw(){
-
-        console.log("je dessin le premier pic");
         //draw the disks on the lines
         for(var i = 0; i < this.towers[0].length; i++){
             console.log(this.towers[0].length - i);
-            var div : HTMLElement = document.getElementById("disk_" + (this.towers[0][i].width).toString() ) as HTMLElement;
+            var div : HTMLElement = document.getElementById("disk_" + (this.towers[0][i]).toString() ) as HTMLElement;
             div.style.bottom = (i) * 45 + 30 - 10 + "px";
             div.style.left = screen.width / 6 - div.offsetWidth / 2 + 5 + "px";
         }
         for(var i = 0; i < this.towers[1].length; i++){
-            var div : HTMLElement = document.getElementById("disk_" + (this.towers[1][i].width).toString() ) as HTMLElement;
+            var div : HTMLElement = document.getElementById("disk_" + (this.towers[1][i]).toString() ) as HTMLElement;
             div.style.bottom = (i) * 45 + 30 - 10 + "px";
             div.style.left = screen.width / 2 - div.offsetWidth / 2 - 5 + "px";
         }
         for(var i = 0; i < this.towers[2].length; i++){
-            var div : HTMLElement = document.getElementById("disk_" + (this.towers[2][i].width).toString() ) as HTMLElement;
+            var div : HTMLElement = document.getElementById("disk_" + (this.towers[2][i]).toString() ) as HTMLElement;
             div.style.bottom = (i) * 45 + 30 - 10 + "px";
             div.style.left = screen.width / 6 * 5 - div.offsetWidth / 2 - 7.5 + "px";
         }
@@ -218,43 +209,5 @@ class Hanoi{
     }
 }
 
-class Disk{
-    width : number;
-    color : string;
-    x : number | null;
-    y : number | null;
-    constructor(width : number, color : string){
-        this.width = width;
-        this.color = color;
-        this.x = null;
-        this.y = null;
-    }
 
-    setX(x : number){
-        this.x = x;
-    }
-
-    setY(y : number){
-        this.y = y;
-    }
-}
-
-class Pic{
-    x : number;
-    y : number;
-    width : number;
-    height : number;
-    color : string;
-    constructor(x : number){
-        this.x = x;
-        this.y = 250;
-        this.width = 10;
-        this.height = 600;
-        this.color = "black";
-    }
-
-    setColor(color : string){
-        this.color = color;
-    }
-}
 
