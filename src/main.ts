f6f457ee922a8ae5e4b9ca2f2770dda6fd27bc47
icepick4 <remi.jara@laposte.net>
2022-05-31 16:13:28 +0200

    shadow with point light

--- src/main.ts
+++ src/main.ts
@@ -18,81 +18,75 @@ var hanoi: Hanoi;
 //import three as a module
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { floorPowerOfTwo } from 'three/src/math/MathUtils';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const pipe1 = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 0.5, 45), new THREE.MeshBasicMaterial({ color: "black" }));
-const pipe2 = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 0.5, 45), new THREE.MeshBasicMaterial({ color: "black" }));
-const pipe3 = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 0.5, 45), new THREE.MeshBasicMaterial({ color: "black" }));
-const pic1 = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, 10, 45), new THREE.MeshBasicMaterial({ color: "gray" }));
-const pic2 = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, 10, 45), new THREE.MeshBasicMaterial({ color: "gray" }));
-const pic3 = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, 10, 45), new THREE.MeshBasicMaterial({ color: "gray" }));
+const pipe1 = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 0.5, 45), new THREE.MeshLambertMaterial({ color: "black" }));
+const pipe2 = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 0.5, 45), new THREE.MeshLambertMaterial({ color: "black" }));
+const pipe3 = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 0.5, 45), new THREE.MeshLambertMaterial({ color: "black" }));
+const pic1 = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, 10, 45), new THREE.MeshStandardMaterial({ color: "gray" }));
+pic1.castShadow = true;
+const pic2 = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, 10, 45), new THREE.MeshStandardMaterial({ color: "gray" }));
+pic2.castShadow = true;
+const pic3 = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, 10, 45), new THREE.MeshStandardMaterial({ color: "gray" }));
+pic3.castShadow = true;
 pipe1.position.x = -15;
 pipe3.position.x = 15
 pic1.position.set(-15, 5, 0);
 pic3.position.set(15, 5, 0);
 pic2.position.y = 5;
-pipe1.receiveShadow = true;
-pipe2.receiveShadow = true;
-pipe3.receiveShadow = true;
-pic1.receiveShadow = true;
-pic2.receiveShadow = true;
-pic3.receiveShadow = true;
+
 
 var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.minAzimuthAngle = Math.PI / 2;
 controls.maxAzimuthAngle = 3 * Math.PI / 2;
 controls.maxPolarAngle = Math.PI / 2 - 0.1;
 
-//floor
-const floor = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshPhongMaterial({ color: "gray", depthWrite: false }));
+// lights
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+hemiLight.position.set(0, 50, 0);
+scene.add(hemiLight);
+
+const dirLight = new THREE.PointLight("rgb(230,230,230)");
+dirLight.position.set(20, 10, -50);
+dirLight.castShadow = true;
+scene.add(dirLight);
+
+
+// ground
+
+const floor = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshStandardMaterial({ color: "rgb(150,150,150)", depthWrite: false }));
 floor.rotation.x = - Math.PI / 2;
 floor.receiveShadow = true;
-floor.position.set(0, -0.5, 0);
-
-//lights
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-hemiLight.position.set(0, 20, 0);
-
-const dirLight = new THREE.DirectionalLight(0xffffff);
-dirLight.position.set(- 3, 10, - 10);
-dirLight.castShadow = true;
-dirLight.shadow.camera.top = 10;
-dirLight.shadow.camera.bottom = - 2;
-dirLight.shadow.camera.left = - 2;
-dirLight.shadow.camera.right = 2;
-dirLight.shadow.camera.near = 0.1;
-dirLight.shadow.camera.far = 10;
-
+scene.add(floor);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 0);
 
-
 //adding elements to the scene
-scene.add(dirLight);
-scene.add(hemiLight);
 scene.add(pipe1);
 scene.add(pipe2);
 scene.add(pipe3);
 scene.add(pic1);
 scene.add(pic2);
 scene.add(pic3);
-scene.add(floor);
 
 
 
 //camera settings
-camera.position.set(10, 15, 50);
+camera.position.set(0, 20, -50);
 controls.update();
-camera.position.z = 5;
 
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
+
 document.body.appendChild(renderer.domElement);
 animate();
 
@@ -100,9 +94,10 @@ animate();
 function initDisks(n: number) {
     for (let i = 0; i < n; i++) {
         var rgb = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
-        var disk = new THREE.Mesh(new THREE.CylinderGeometry(n - i + 0.5, n - i + 0.5, 1.5, 45), new THREE.MeshLambertMaterial({ color: rgb }));
+        var disk = new THREE.Mesh(new THREE.CylinderGeometry(n - i + 0.5, n - i + 0.5, 1.5, 45), new THREE.MeshStandardMaterial({ color: rgb }));
         disk.position.set(15, i * 1.5 + 1.5 / 2, 0);
         disk.receiveShadow = true;
+        disk.castShadow = true;
         scene.add(disk);
     }
 }
